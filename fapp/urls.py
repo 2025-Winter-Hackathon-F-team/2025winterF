5e5a4b0a6dfa8dc898904c53402728709614347e
Sen <ffix.3000.c@gmail.com>
2025-01-25 11:25:15 +0900

    #38 fappのhello_mysql.htmlにbase.cssを反映させる

--- fapp/urls.py
+++ fapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import hello_world, hello_mysql
 
+
 urlpatterns = [
     path('', hello_world),
     path('mysql/', hello_mysql)
--- fapp/views.py
+++ fapp/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Message
 
-def hello_world(requests):
+
+def hello_world(request):
     return HttpResponse("Hello World!")
 
+
 def hello_mysql(request):
-    message = Message.objects.first() #最初のメッセージを取得
-    return render(request, 'hello_mysql.html', {"message":message})
+    message = Message.objects.first() #fapp/models.pyから最初のメッセージを取得
+    if message is None:
+        message = {"text": "No messages found"}
+    return render(request, 'hello_mysql.html', {"message": message})
