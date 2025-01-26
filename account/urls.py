4c6ee6367c3a39e56991a42915eb4f7b15bee2eb
takumu <takumu155695@gmail.com>
2025-01-26 15:11:48 +0900

    #6 ホーム画面のログアウトボタンを押してログアウトを行うことができる

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import SignUpView, LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path("login/", LoginView.as_view(), name="login")
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
