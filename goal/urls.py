4a9e18f74f427b20995561f95f304cc8331c9cb2
takumu <takumu155695@gmail.com>
2025-01-29 22:07:32 +0900

    #50 名前空間の適用

--- goal/urls.py
+++ goal/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import HomeView
 
+# 名前空間を設定
+app_name = 'goal'
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
