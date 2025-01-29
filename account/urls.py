2ad8016cb05de30b80843f38d093e623637eb27f
takumu <takumu155695@gmail.com>
2025-01-29 22:05:22 +0900

    #50 urlを設定

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import SignUpView, LoginView
+from .views import InitialSetupView, SignUpView, LoginView
 from django.contrib.auth.views import LogoutView
+
+# 名前空間を設定
+app_name = 'account'
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("initial_setup/", InitialSetupView.as_view(), name="initial_setup"),
 ]
