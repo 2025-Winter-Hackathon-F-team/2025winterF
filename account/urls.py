28e03e511512505c2c454e1b9e7a9aa6654641b7
takumu <takumu155695@gmail.com>
2025-01-31 22:53:14 +0900

    #58 ユーザープロフィール画面を作成する

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import InitialSetupView, SignUpView, LoginView
+from .views import InitialSetupView, SignUpView, LoginView, ProfileView
 from django.contrib.auth.views import LogoutView
 
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("initial_setup/", InitialSetupView.as_view(), name="initial_setup"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,12 @@
 from django.urls import reverse_lazy
+from datetime import date
 from django.views.generic.edit import FormView, UpdateView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import DatabaseError, IntegrityError
 from django.core.exceptions import ValidationError
+from dateutil.relativedelta import relativedelta
 
 from .forms import InitialSetupForm, SignUpForm, LoginForm
 from .models import User
@@ -91,3 +94,39 @@ class InitialSetupView(LoginRequiredMixin, UpdateView):
         # エラーが発生した場合、フォームを含むレスポンスを返す
         return self.form_invalid(form)
 
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    template_name = "profile.html"
+    model = User
+
+    def get_object(self):
+        """
+        現在ログインしているユーザーオブジェクトを取得する。
+        Returns:
+            User: ログイン中のユーザー。
+        """
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        """
+        テンプレートに渡すコンテキストデータを取得する。
+        Returns:
+            dict: コンテキストデータ。
+        """
+        context = super().get_context_data(**kwargs)
+        # get_objectで取得したユーザーオブジェクト
+        user = self.object
+        # ユーザーの年齢と寿命を計算
+        context['age'] = (
+            relativedelta(date.today(), user.birthday).years
+            if user.birthday
+            else None
+        )
+
+        context['life_span'] = (
+            relativedelta(user.deathday, user.birthday).years
+            if user.birthday and user.deathday
+            else None
+        )
+
+        return context
