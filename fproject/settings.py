ffc6e62c266502f9cdc5b7b298502ab5f497bd7b
takumu <takumu155695@gmail.com>
2025-02-08 11:09:44 +0900

    #54 ターミナルにログを出力できるように設定

--- fproject/settings.py
+++ fproject/settings.py
@@ -135,3 +135,18 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# ログの設定（開発用）
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
