805c050e0025d5bf69336bef98f53a35d70c6dd5
Sen <ffix.3000.c@gmail.com>
2025-02-09 10:55:49 +0900

    #64 フィードバック画面デザインたたき台

--- goal/urls.py
+++ goal/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView, CreateYearGoalView
-from .views import CreateYearGoalView
+from .views import HomeView, CreateYearGoalView, FeedbackView
+
 
 # 名前空間を設定
 app_name = "goal"
@@ -9,4 +9,5 @@ app_name = "goal"
 urlpatterns = [
     path("year_goal/", CreateYearGoalView.as_view(), name="year_goal"),
     path("", HomeView.as_view(), name="home"),
+    path("feedback/", FeedbackView.as_view(), name="feedback"),
 ]
