01777a015449a94b89b5e80b4431b89cfde9012f
souyama77 <p14p083@yahoo.co.jp>
2025-02-16 19:25:03 +0900

    #73 年目標複数登録のバリデーション処理

--- goal/forms/year_goal/year_goal_forms.py
+++ goal/forms/year_goal/year_goal_forms.py
@@ -1,12 +1,17 @@
 from ...models import YearGoal
 from django import forms
-import datetime
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 class YearGoalForm(forms.ModelForm):
     class Meta:
         model = YearGoal
         fields = ["title"]
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request", None)  # requestを受け取る
+        super().__init__(*args, **kwargs)
 
     def clean_title(self):
         # タイトルの値を確認
@@ -28,5 +33,21 @@ class YearGoalForm(forms.ModelForm):
             raise forms.ValidationError("タイトルは50文字以内で入力してください。")
         return title
 
-    # def clean(self):
-    #     return super().clean()
+    def clean(self):
+        cleaned_data = super().clean()
+        if self.request and self.request.user.is_authenticated:
+            user = self.request.user
+            # 現在の年の最初の日付と最後の日付を取得
+            current_year = timezone.now().year
+            current_year_str = f"{current_year}-01-01"  # 文字列に変換
+            if YearGoal.objects.filter(user=user, year=current_year_str).exists():
+                raise ValidationError(f"{current_year}年の目標は既に登録されています。")
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if self.request and self.request.user.is_authenticated:
+            instance.user = self.request.user
+        if commit:
+            instance.save()
+        return instance
--- goal/views/year_goal/create_year_goal_views.py
+++ goal/views/year_goal/create_year_goal_views.py
@@ -1,11 +1,11 @@
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormView
-
+import datetime
 from ...forms import YearGoalForm
 from ...models import YearGoal, MonthGoal
 
-import logging #TODO: コミット前に削除
+import logging  # TODO: コミット前に削除
 
 logger = logging.getLogger(__name__)
 
@@ -20,10 +20,22 @@ class CreateYearGoalView(LoginRequiredMixin, FormView):
     template_name = "year_goal_create.html"
     success_url = reverse_lazy("goal:home")
 
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["request"] = self.request
+        return kwargs
+
     def form_valid(self, form):
+
         user = self.request.user
+        if not user.is_authenticated:
+            return self.handle_no_permission()
         # 年目標を作成
-        year_goal = YearGoal.objects.create(user=user, title=form.cleaned_data["title"])
+        year_goal = form.save()  # form.save()を使用
+        year_goal.user = user
+        year_goal.year = datetime.date(datetime.datetime.now().year, 1, 1)
+        year_goal.save()
+
         # 月目標を作成
         MonthGoal.create_monthly_goals_for_year(year_goal.id)
         return super().form_valid(form)
