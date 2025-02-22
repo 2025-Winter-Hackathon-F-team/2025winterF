072dc9a0ef3802873e2cb48edc7c79d32dfce3ab
souyama77 <p14p083@yahoo.co.jp>
2025-02-22 13:39:52 +0900

    月目標編集機能 #91

--- goal/urls.py
+++ goal/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 
-from .views import HomeView, CreateYearGoalView, YearGoalDetailView, YearGoalUpdateView, MontGoalDetailView, MonthGoalAchieveView, TodoCreateView, TodoDetailView, UnachievedTodoCheckView, FeedbackView
+from .views import (
+    HomeView,
+    CreateYearGoalView,
+    YearGoalDetailView,
+    YearGoalUpdateView,
+    MontGoalDetailView,
+    MonthGoalAchieveView,
+    MonthGoalUpdateView,
+    TodoCreateView,
+    TodoDetailView,
+    UnachievedTodoCheckView,
+    FeedbackView,
+)
 
 
 # 名前空間を設定
@@ -11,25 +23,70 @@ urlpatterns = [
     # 年指定なしでアクセス（今年の目標）
     path("year_goal/detail/", YearGoalDetailView.as_view(), name="year_goal_detail"),
     # 年を指定した場合
-    path("year_goal/detail/<int:year>/", YearGoalDetailView.as_view(), name="year_goal_detail_year"),
+    path(
+        "year_goal/detail/<int:year>/",
+        YearGoalDetailView.as_view(),
+        name="year_goal_detail_year",
+    ),
     # 年目標のタイトルを更新
-    path("year_goal/<int:year>/edit/", YearGoalUpdateView.as_view(), name="year_goal_edit"),
+    path(
+        "year_goal/<int:year>/edit/",
+        YearGoalUpdateView.as_view(),
+        name="year_goal_edit",
+    ),
     # 年指定なしでアクセス（月の目標）
-    path("year_goal/month_goal/<int:month>/", MontGoalDetailView.as_view(), name="month_goal_detail"),
+    path(
+        "year_goal/month_goal/<int:month>/",
+        MontGoalDetailView.as_view(),
+        name="month_goal_detail",
+    ),
     # 年を指定した場合（月の目標）
-    path("year_goal/<int:year>/month_goal/<int:month>/", MontGoalDetailView.as_view(), name="month_goal_detail_specific_year"),
+    path(
+        "year_goal/<int:year>/month_goal/<int:month>/",
+        MontGoalDetailView.as_view(),
+        name="month_goal_detail_specific_year",
+    ),
+    path(
+        "year_goal/month_goal/<int:year>/<int:month>/edit/",
+        MonthGoalUpdateView.as_view(),
+        name="month_goal_edit",
+    ),
     # Todo作成画面（年指定なし）
-    path("year_goal/month_goal/<int:month>/todo/create/", TodoCreateView.as_view(), name="create_todo"),
+    path(
+        "year_goal/month_goal/<int:month>/todo/create/",
+        TodoCreateView.as_view(),
+        name="create_todo",
+    ),
     # Todo作成画面（年指定あり）
-    path("year_goal/<int:year>/month_goal/<int:month>/todo/create/", TodoCreateView.as_view(), name="create_todo_specific_year"),
+    path(
+        "year_goal/<int:year>/month_goal/<int:month>/todo/create/",
+        TodoCreateView.as_view(),
+        name="create_todo_specific_year",
+    ),
     # 月目標の状態を達成に変更する
-    path("year_goal/<int:year>/month_goal/<int:month>/achieve/", MonthGoalAchieveView.as_view(), name="month_goal_achieve"),
+    path(
+        "year_goal/<int:year>/month_goal/<int:month>/achieve/",
+        MonthGoalAchieveView.as_view(),
+        name="month_goal_achieve",
+    ),
     # 月目標に紐づく未達成のTodoがあるか確認
-    path("year_goal/<int:year>/month_goal/<int:month>/has_unachieved_todos/", UnachievedTodoCheckView.as_view(), name="has_unachieved_todos"),
+    path(
+        "year_goal/<int:year>/month_goal/<int:month>/has_unachieved_todos/",
+        UnachievedTodoCheckView.as_view(),
+        name="has_unachieved_todos",
+    ),
     # Todo詳細画面（年指定なし）
-    path("year_goal/month_goal/<int:month>/todo/<int:todo>", TodoDetailView.as_view(), name="todo_detail"),
+    path(
+        "year_goal/month_goal/<int:month>/todo/<int:todo>",
+        TodoDetailView.as_view(),
+        name="todo_detail",
+    ),
     # Todo詳細画面（年指定あり）
-    path("year_goal/<int:year>/month_goal/<int:month>/todo/<int:todo>", TodoDetailView.as_view(), name="todo_detail_specific_year"),
+    path(
+        "year_goal/<int:year>/month_goal/<int:month>/todo/<int:todo>",
+        TodoDetailView.as_view(),
+        name="todo_detail_specific_year",
+    ),
     path("", HomeView.as_view(), name="home"),
     path("feedback/", FeedbackView.as_view(), name="feedback"),
 ]
