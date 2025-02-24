a8200fd85036fe90a93377dc4d9fbea37e023b67
Sen <ffix.3000.c@gmail.com>
2025-02-24 12:04:11 +0900

    月詳細→ToDo詳細へのリンク用意、ToDo関連リストに新規ボタン用意 #104

---
+++ static/js/todo_create.js
@@ -0,0 +1,13 @@
+var $btn = document.getElementById("submit");
+var $form = document.getElementById("form");
+
+function login() {
+  if ($form.checkValidity()) {
+    $btn.classList.add("pending");
+    window.setTimeout(function () {
+      $btn.classList.add("granted");
+    }, 1500);
+  }
+}
+
+$btn.addEventListener("click", todo_create);
